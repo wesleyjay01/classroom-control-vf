70b895faa62a95d842e960b174a022d58733192d
wesleyjay01 <wesleyjay@yahoo.com>
2016-04-25 17:01:37 -0600

    Modify motd

--- manifests/site.pp
+++ manifests/site.pp
@@ -44,3 +44,11 @@ node default {
   #   class { 'my_class': }
   notify { "Hello, my name is ${::hostname}": }
 }
+
+file { '/etc/motd':
+  ensure  => file,
+  owner   => 'root',
+  group   => 'root',
+  mode    => '0644',
+  content => "Hey, Puppet is so awesome!\n",
+}
