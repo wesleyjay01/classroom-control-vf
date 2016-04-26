bcaeed81bd58c1f0deb64c2093b2bc46d0462759
wesleyjay01 <wesleyjay@yahoo.com>
2016-04-26 13:32:01 -0600

    Update init.pp

--- site/skeleton/manifests/init.pp
+++ site/skeleton/manifests/init.pp
@@ -9,6 +9,6 @@ class skeleton {
   file { '/etc/skel/.bashrc':
     ensure  =>  file,
     owner   => 'root',
-    source  => puppet:///modules/skeleton/bashrc',
+    source  => 'puppet:///modules/skeleton/bashrc',
     }
 }
