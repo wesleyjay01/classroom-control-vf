4b721d8610bc5aff570632bb3b3ee5dec04535fb
wesleyjay01 <wesleyjay@yahoo.com>
2016-04-26 17:02:20 -0600

    Update init.pp

--- site/nginx/manifests/init.pp
+++ site/nginx/manifests/init.pp
@@ -1,3 +1,36 @@
 #
 class nginx {
+  package {'nginx':
+    ensure => present,
+    }
+
+  file {'/etc/sysconfig/memcached':
+    ensure => file,
+    source  => 'puppet:///modules/memcached/memcached',
+    require => Package['memcached'],
+    }
+
+  service { 'nginx':
+    ensure => running,
+    enable => true,
+    subscribe => File['/etc/sysconfig/memcached'],
+  }
+
+}
+
+
+class memcached {
+  package {'memcached':
+    ensure => present,
+    }
+  file {'/etc/sysconfig/memcached':
+    ensure => file,
+    source  => 'puppet:///modules/memcached/memcached',
+    require => Package['memcached'],
+    }
+  service { 'memcached':
+    ensure => running,
+    enable => true,
+    subscribe => File['/etc/sysconfig/memcached'],
+  }
 }
