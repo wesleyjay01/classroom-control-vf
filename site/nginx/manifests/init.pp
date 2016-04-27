4ac070a5a4371956f08be01988f1c04564012be5
wesleyjay01 <wesleyjay@yahoo.com>
2016-04-27 09:45:37 -0600

    Update init.pp

--- site/nginx/manifests/init.pp
+++ site/nginx/manifests/init.pp
@@ -8,31 +8,32 @@ class nginx {
     ensure => directory,
   }
 
+  file {'/etc/nginx/conf.d':
+    ensure => directory,
+  }
+
   file {'/var/www/index.html':
     ensure => file,
     source => 'puppet:///modules/nginx/files/index.html',
   }
 
-  file {'/etc/nginx/nginx.conf':
-    ensure => file,
-    source => 'puppet:///modules/nginx/files/nginx.conf',
-    require => Package['nginx'],
-    notify => Service['nginx'],
-  }
+#  file {'/etc/nginx/nginx.conf':
+#    ensure => file,
+#    source => 'puppet:///modules/nginx/files/nginx.conf',
+3    require => Package['nginx'],
+#    notify => Service['nginx'],
+#  }
   
-  file {'/etc/nginx/conf.d':
-    ensure => directory,
-  }
+
+#  file {'/etc/nginx/conf.d/default.conf':
+#    ensure => file,
+#    source => 'puppet:///modules/nginx/files/default.conf',
+#    require => Package['nginx'],
+#    notify => Service['nginx'],
+#  }
   
-  file {'/etc/nginx/conf.d/default.conf':
-    ensure => file,
-    source => 'puppet:///modules/nginx/files/default.conf',
-    require => Package['nginx'],
-    notify => Service['nginx'],
-  }
-  
-  service { 'nginx':
-    ensure => running,
-    enable => true,
-  }
+#  service { 'nginx':
+#    ensure => running,
+#    enable => true,
+#  }
 }
